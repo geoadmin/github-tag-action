0b8d5c0bc48d17b9a97a5a9e8034ec07b246425d
Benjamin Sugden <benjamin.sugden@swisstopo.ch>
2024-04-25 11:15:11 +0200

    Fix deprecated output

--- entrypoint.sh
+++ entrypoint.sh
@@ -107,7 +107,7 @@ commit=$(git rev-parse HEAD)
 if [[ "${tag_commit}" == "${commit}" ]]
 then
     echo "No new commits since previous tag. Skipping..."
-    echo "::set-output name=tag::${tag}"
+    echo "tag=${tag}" >> "${GITHUB_OUTPUT}"
     exit 0
 fi
 
@@ -123,16 +123,16 @@ case "${log}" in
     *#patch* ) new=$(semver -i patch "${tag}"); part="patch";;
     *#none* )
         echo "Default bump was set to none. Skipping..."
-        echo "::set-output name=new_tag::${tag}"
-        echo "::set-output name=tag::${tag}"
+        echo "new_tag=${tag}" >> "${GITHUB_OUTPUT}"
+        echo "tag=${tag}" >> "${GITHUB_OUTPUT}"
         exit 0
         ;;
     * )
         if [[ "${default_semvar_bump}" == "none" ]]
         then
             echo "Default bump was set to none. Skipping..."
-            echo "::set-output name=new_tag::${tag}"
-            echo "::set-output name=tag::${tag}"
+            echo "new_tag=${tag}" >> "${GITHUB_OUTPUT}"
+            echo "tag=${tag}" >> "${GITHUB_OUTPUT}"
             exit 0
         else
             new=$(semver -i "${default_semvar_bump}" "${tag}"); part=${default_semvar_bump}
@@ -173,17 +173,17 @@ else
 fi
 
 # set outputs
-echo "::set-output name=new_tag::${new}"
-echo "::set-output name=part::${part}"
+echo "new_tag=${new}" >> "${GITHUB_OUTPUT}"
+echo "part=${part}" >> "${GITHUB_OUTPUT}"
 
 # use dry run to determine the next tag
 if ${dryrun}
 then
-    echo "::set-output name=tag::${tag}"
+    echo "tag=${tag}" >> "${GITHUB_OUTPUT}"
     exit 0
 fi
 
-echo "::set-output name=tag::${new}"
+echo "tag=${new}" >> "${GITHUB_OUTPUT}"
 
 # create local git tag
 git tag "${new}"
